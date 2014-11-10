32f3da0918574e75468c6d9931384a4a769f8876
niwinnm <niwin93@gmail.com>
2014-11-11 00:32:38 +0530

    Update Shadow_Selection.user.js

--- Shadow_Selection.user.js
+++ Shadow_Selection.user.js
@@ -4,6 +4,10 @@
 // @description Use this script to change your Browser's boring text selection color to a "Shadow based selection style"
 // @include     *
 // @version     1
+// @copyright	  2014, Anveshak
+// @supportURL  mailto:niwin93@gmail.com
+// @icon 		     http://imageshack.com/a/img910/9717/tlnJGD.png
+// @oujs:author Anveshak
 // @grant       none
 // ==/UserScript==
  var style=document.createElement("style");
