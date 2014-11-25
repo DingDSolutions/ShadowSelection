31a20c4e6a8f6ec137466ca544cd7bf9c731109b
Niwin Santhosh <niwin93@gmail.com>
2014-11-25 14:03:56 +0530

    Update Shadow_Selection.user.js

--- Shadow_Selection.user.js
+++ Shadow_Selection.user.js
@@ -13,7 +13,10 @@
 // ==/UserScript==
  var style=document.createElement("style");
 // edit the text-shadow property to change the Selection color and Shadow radius
-    style.innerHTML ="/*Firefox Browser*/ ::-moz-selection { color: #16A; background: none repeat scroll 0% 0% transparent;text-shadow: 2px 0px 2px #000;}";
-    style.innerHTML +="/* WebKit/Blink Browsers */ ::selection { color: #16A; background: none repeat scroll 0% 0% transparent;text-shadow: 2px 0px 2px #000;}";
+// #328DFB is used as default Shadow Selection style.
+// Use #F07746 for Ubuntu's firefox selection style. 
+
+    style.innerHTML ="/*Firefox Browser*/ ::-moz-selection { color: #000; background: none repeat scroll 100% 0% transparent;text-shadow: 2px 0px 2px #328DFB;}";
+    style.innerHTML +="/* WebKit/Blink Browsers */ ::selection { color: #000; background: none repeat scroll 0% 0% transparent;text-shadow: 2px 0px 2px #328DFB;}";
     
 document.body.appendChild(style); 
