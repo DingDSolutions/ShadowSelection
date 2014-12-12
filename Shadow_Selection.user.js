26be89977da2c22e64a44e5229a0408dd89d1c9f
Niwin Santhosh <niwin93@gmail.com>
2014-12-12 19:23:55 +0530

    Update Shadow_Selection.user.js

--- Shadow_Selection.user.js
+++ Shadow_Selection.user.js
@@ -19,9 +19,9 @@
 // Use #EA4200 (Ubuntu's orange color) for Ubuntu's firefox. 
 
 //For Firefox (Gecko) Browser
-    style.innerHTML = "::-moz-selection { color: #000; background: none repeat scroll 100% 0% transparent;text-shadow: 0px 0px 2px #352E7E;}";
+    style.innerHTML = "::-moz-selection { color: #000; background: none repeat scroll 100% 0% transparent;text-shadow: 0px 0px 2px #3356C6;}";
 //For Chrome (Blink) Browser
-    style.innerHTML +=" ::selection { color: #000; background: none repeat scroll 0% 0% transparent;text-shadow: 0px 0px 2px #352E7E;}  input::selection { color: #328DFB; background: none repeat scroll 0% 0% transparent;}";
+    style.innerHTML +=" ::selection { color: #000; background: none repeat scroll 0% 0% transparent;text-shadow: 0px 0px 2px #352E7E;}  input::selection { color: #328DFB; background: none repeat scroll 0% 0% transparent;} a::selection { color: #000; background: none repeat scroll 0% 0% transparent;text-shadow: 2px 0px 2px #352E7E;}";
 //For WebKit Browser
     style.innerHTML +=" ::-webkit-selection { color: #000; background: none repeat scroll 0% 0% transparent;text-shadow: 0px 0px 2px #352E7E;}";
 //For Other Browsers
